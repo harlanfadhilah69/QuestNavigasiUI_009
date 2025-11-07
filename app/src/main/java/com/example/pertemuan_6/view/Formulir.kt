1c7c51f7f2f9e2692f6c421723a71a2fac077ef1
harlanfadhilah69 <harlancoegg@gmail.com>
2025-11-07 15:41:23 +0700

    menambahkan OutlinedTextField untuk input Nama Lengkap pada FormIsian

--- app/src/main/java/com/example/pertemuan_6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan_6/view/Formulir.kt
@@ -34,5 +34,28 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
-
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id= R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700))
+            ) }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
+        }
+    }
 }
